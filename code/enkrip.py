77cf997763d094ae56e629e84e680b9d8bfc34b5
mufidabiyyu5 <73393247+mufidabiyyu5@users.noreply.github.com>
2022-11-05 19:24:55 +0700

    Update enkrip.py

--- code/enkrip.py
+++ code/enkrip.py
@@ -1,9 +1,10 @@
-# Generate vector for the key
+# Generate vector untuk kunci
 keyMatrix = [[0] * 3 for i in range(3)]
-# Generate vector for the message
-messageVector = [[0] for i in range(3)]
-# Generate vector for the cipher
+# Generate vector untuk plain text
+textVector = [[0] for i in range(3)]
+# Generate vector untuk cipher text
 cipherMatrix = [[0] for i in range(3)]
+
 
 def generateKey(string, key):
     key = list(key)
@@ -15,21 +16,23 @@ def generateKey(string, key):
             key.append(key[i % len(key)])
     return("" . join(key))
 
+
 def shiftcipher(string, key):
-    """Encrypts text using a Caesar cipher.
+    """Enkripsi dengan Caesar Cipher
     """
     key = len(key)
     cipher_text = []
     for i in range(len(string)):
         char = string[i]
-        if (char.isupper()):##UNICODE 
+        if (char.isupper()):  # UNICODE
             cipher_text += chr((ord(char) + key - 65) % 26 + 65)
         else:
             cipher_text += chr((ord(char) + key - 97) % 26 + 97)
     return cipher_text
 
-def viginerecipher(string, key):
-    """Encrypts text using a Viginere cipher.
+
+def vigenerecipher(string, key):
+    """Enkripsi dengan Vigenere Chiper
     """
     cipher_text = []
     for i in range(len(string)):
@@ -39,62 +42,62 @@ def viginerecipher(string, key):
         cipher_text.append(chr(x))
     return("" . join(cipher_text))
 
+
 def getKeyMatrix(key):
     k = 0
     for i in range(3):
         for j in range(3):
             keyMatrix[i][j] = ord(key[k]) % 65
             k += 1
- 
-# Following function encrypts the message
-def encrypt(messageVector):
+
+# Fungsi untuk mengenkripsi kan text
+def encrypt(textVector):
     for i in range(3):
         for j in range(1):
             cipherMatrix[i][j] = 0
             for x in range(3):
                 cipherMatrix[i][j] += (keyMatrix[i][x] *
-                                       messageVector[x][j])
+                                       textVector[x][j])
             cipherMatrix[i][j] = cipherMatrix[i][j] % 26
- 
+
+
 def HillCipher(message, key):
- 
-    # Get key matrix from the key string
+
+    # Mendapatkan matrix kunci dari string kunci
     getKeyMatrix(key)
- 
-    # Generate vector for the message
+
+    # Generate vector untuk plain text
     for i in range(3):
-        messageVector[i][0] = ord(message[i]) % 65
- 
-    # Following function generates
-    # the encrypted vector
-    encrypt(messageVector)
- 
-    # Generate the encrypted text
-    # from the encrypted vector
+        textVector[i][0] = ord(message[i]) % 65
+
+    # Memanggil fungsi untuk mengenkripsi
+    encrypt(textVector)
+
+    # Generate text yang sudah di enkripsi dari vector
     CipherText = []
     for i in range(3):
         CipherText.append(chr(cipherMatrix[i][0] + 65))
- 
-    # Finally print the ciphertext
-    # print("Ciphertext: ", "".join(CipherText))
+
+    # Return hasil cipher text
     return "".join(CipherText)
 
+
 def main():
-    text = input("Enter text: ") 
-    key = input("Enter key: ")
+    text = raw_input("Enter text: ")
+    key = raw_input("Enter key: ")
     key = generateKey(text, key)
     ciphertext = shiftcipher(text, key)
-    ciphertext = viginerecipher(ciphertext, key)
-    ciphertext_list = [text[i:i+3] for i in range(0, len(text), 3)]
+    ciphertext = vigenerecipher(ciphertext, key)
+    ciphertext_list = [ciphertext[i:i+3] for i in range(0, len(text), 3)]
     hasil = ''
     for i in ciphertext_list:
         if len(i) < 3:
             i = i + "X" * (3 - len(i))
         hasil += HillCipher(i, key)
-    
+
     print("Ciphertext: ", hasil)
     # print("Ciphertext :", ciphertext)
-    
+
 
 if __name__ == "__main__":
     main()
